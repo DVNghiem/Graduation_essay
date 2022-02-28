7bdd8a464a45a9c5b630227de294ff885ae51070
nghiemIUH <vannghiem848@gmail.com>
2022-02-28 08:45:07 +0700

    remove docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,7 +131,7 @@ AUTH_USER_MODEL = 'User.CustomUser'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/code/static/"
+STATIC_ROOT = "/home/ubuntu/WebDoAn/backend/static"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
