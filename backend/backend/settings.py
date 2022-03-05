42b793fed6e1c6d6b994034b177a6dfb3af8b04c
nghiemIUH <vannghiem848@gmail.com>
2022-03-05 12:39:17 +0700

    update static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,10 +131,7 @@ AUTH_USER_MODEL = 'User.CustomUser'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/ubuntu/WebDoAn/backend/static"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
