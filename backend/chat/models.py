174945034c6f3e0bd9c4f7a66d903a3f67362428
nghiemIUH <vannghiem848@gmail.com>
2022-03-21 15:47:52 +0700

    fix migrate models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,9 +1,4 @@
-import imp
-from statistics import mode
 from django.db import models
-from django.contrib.auth import get_user_model
-from datetime import datetime
-from User.models import UserRelationship
 # Create your models here.
 
 
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,6 +1,3 @@
-from cgi import test
-import imp
-import re
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
@@ -9,19 +6,19 @@ from . import models
 from . import serializers
 # Create your views here.
 
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def addChat(request):
     d = request.data['content']
-    # try:
-    models.ChatContent.objects.create(
-        content = d
-    )
-    return Response(data='oke', status = status.HTTP_200_OK)
-    # except:
-    #     return Response(data='no oke', status = status.HTTP_400_BAD_REQUEST)
-    
-  
+    try:
+        models.ChatContent.objects.create(
+            content=d
+        )
+        return Response(data='oke', status=status.HTTP_200_OK)
+    except:
+        return Response(data='no oke', status=status.HTTP_400_BAD_REQUEST)
+
 
 # @api_view(['POST'])
 # @permission_classes([AllowAny])
